7eeb0b390e31cee6e99541d37b8a0c8ac94b01e3
mayckl2 <mayckluciano2@gmail.com>
2026-02-01 14:26:52 -0300

    ✨ feat: Conection database
    conecting webapi with sqlserver database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using TaskList.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TaskContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
