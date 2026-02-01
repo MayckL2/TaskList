b8ba3f77a78339d3eddb66a0cf2fca0d7c5207f7
mayckl2 <mayckluciano2@gmail.com>
2026-02-01 14:30:05 -0300

    ✨ feat: setting context

--- Contexts/TaskContext.cs
+++ Contexts/TaskContext.cs
@@ -1,6 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskList.Entities;
+
 namespace TaskList.Contexts;
 
-public class TaskContext
+public class TaskContext : DbContext
 {
-    
+    public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TaskEntitie> Tasks { get; set; }
+
+// specify the id as the primary key in the entity
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskEntitie>()
+            .HasKey(t => t.Id);
+    }
 }
