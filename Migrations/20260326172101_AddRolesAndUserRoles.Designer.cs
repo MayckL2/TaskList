f34a2d833a519bfb270842165f2a2af6d8c02a82
mayckl2 <mayckluciano2@gmail.com>
2026-03-26 14:54:08 -0300

    ✨ feat: update migrations
    Add new tables for authentication

--- Migrations/20260326172101_AddRolesAndUserRoles.Designer.cs
+++ Migrations/20260326172101_AddRolesAndUserRoles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskList.Contexts;
 
@@ -11,9 +12,11 @@ using TaskList.Contexts;
 namespace TaskList.Migrations
 {
     [DbContext(typeof(TaskContext))]
-    partial class TaskContextModelSnapshot : ModelSnapshot
+    [Migration("20260326172101_AddRolesAndUserRoles")]
+    partial class AddRolesAndUserRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
