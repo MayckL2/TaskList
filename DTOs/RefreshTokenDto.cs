57d57feb235186b8a30e3a081da5f33650f7befb
mayckl2 <mayckluciano2@gmail.com>
2026-06-09 12:53:45 -0300

    🐞 fix: Duplicated error
    Fixing duplicated error, files duplicate after changing folder architecture

--- DTOs/RefreshTokenDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace TaskList.DTOs;
-
-public class RefreshTokenDto
-{
-    [Required(ErrorMessage = "Refresh token é obrigatório")]
-    public string RefreshToken { get; set; } = string.Empty;
-}
