24c3640eb2b655e8bca2ccb87e05835b7d2db309
mayckl2 <mayckluciano2@gmail.com>
2026-09-15 15:43:43 -0300

    🦄 refactor: Adjusting dependency
    Implementation of repository to her interface

--- TaskList.API/Services/TaskService.cs
+++ TaskList.API/Services/TaskService.cs
@@ -10,9 +10,9 @@ namespace TaskList.Services;
 
 public class TaskService : ITaskService
 {
-    private readonly TaskRepository _repository;
+    private readonly ITaskRepository _repository;
 
-    public TaskService(TaskRepository repository)
+    public TaskService(ITaskRepository repository)
     {
         _repository = repository;
     }
